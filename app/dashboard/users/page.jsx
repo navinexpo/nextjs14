4e640affd04746e0b810d934b6b450d45b2d6850
navin <navinexpo2020@gmail.com>
2024-01-10 18:08:26 +0530

    added and update the user page

--- app/dashboard/users/page.jsx
+++ app/dashboard/users/page.jsx
@@ -8,7 +8,7 @@ const UserPage = () => {
   return (
     <div className={styles.container}>
       <div className={styles.top}>
-        <Search placeholder="Search for a user ..." />
+        <Search placeholder="Search for User..." />
         <Link href="/dashboard/users/add">
           <button className={styles.addButton}>Add New</button>
         </Link>
@@ -20,40 +20,41 @@ const UserPage = () => {
             <td>Email</td>
             <td>Created At</td>
             <td>Role</td>
-            <td>Status</td>
             <td>Action</td>
           </tr>
-          <tbody>
-            <tr>
-              <td>
-                <div className={styles.user}>
-                  <Image
-                    src="/noavator.png"
-                    alt=""
-                    width={40}
-                    height={40}
-                    className={styles.userImage}
-                  />
-                  Navin Kukreja
-                </div>
-              </td>
-              <td>navin@gmail.com</td>
-              <td>12.01.2024</td>
-              <td>Admin</td>
-              <td>active</td>
-              <td>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <div className={styles.user}>
+                <Image
+                  src="/noavatar.png"
+                  alt=""
+                  width={40}
+                  height={40}
+                  className={styles.userImage}
+                />{" "}
+                Navin Kukreja
+              </div>
+            </td>
+            <td>navin@gmail.com</td>
+            <td>14.12.2024</td>
+            <td>Admin</td>
+            <td>Active</td>
+            <td>
+              <div className={styles.buttons}>
                 <Link href="/">
                   <button className={`${styles.button} ${styles.view}`}>
                     View
                   </button>
                 </Link>
-                <button
-                  className={`${styles.button} ${styles.delete}`}
-                ></button>
-              </td>
-            </tr>
-          </tbody>
-        </thead>
+                <button className={`${styles.button} ${styles.delete}`}>
+                  Delete
+                </button>
+              </div>
+            </td>
+          </tr>
+        </tbody>
       </table>
     </div>
   );
